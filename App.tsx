3311df380275796ca18a3a07829b49006797aec9
anindomac <sarkeranindo@gmail.com>
2023-03-22 13:09:45 +0600

    twind ok

--- App.tsx
+++ App.tsx
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-   <View className="flex-1 items-center justify-center bg-red-500">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <SafeAreaView className="flex-1 items-center justify-center p-4 rounded-2xl bg-green-100">
+        <Text>Open up App.js to start working on your app!</Text>
+        <View className="bg-amber-100 rounded-2xl border text-white">
+          <Button
+            title="Slim shady"
+            onPress={() => {
+              console.log("I'm slim shady");
+            }}
+          />
+        </View>
+
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
