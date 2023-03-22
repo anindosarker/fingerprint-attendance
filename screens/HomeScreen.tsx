157bfc6cd284f135934410317010c6dcc73fa974
anindomac <sarkeranindo@gmail.com>
2023-03-22 18:01:34 +0600

    network works

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as LocalAuthentication from "expo-local-authentication";
+import * as Network from "expo-network";
 
 export default function HomeScreen() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,6 +11,17 @@ export default function HomeScreen() {
     console.log("auth result", result);
     setIsLoggedIn(result.success);
   }
+
+  const [ip, setIp] = useState(null);
+
+  async function getNetInfo() {
+    const networkInfo = await Network.getIpAddressAsync();
+    setIp(networkInfo);
+  }
+
+  useEffect(() => {
+    getNetInfo();
+  }, [ip]);
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -36,6 +48,7 @@ export default function HomeScreen() {
           />
         </View>
       )}
+      <Text>Home Screen ip: {ip}</Text>
     </View>
   );
 }
